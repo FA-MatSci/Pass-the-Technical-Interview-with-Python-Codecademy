1de324781adfa674b8fa7fd6a49e9ebadb6696e1
FA-MatSci <filip.ambroz5@gmail.com>
2023-07-01 17:27:38 +0200

    Add files via upload

---
+++ 7. Graphs and Graph Traversal/Graphs/graphs_vertex.py
@@ -0,0 +1,10 @@
+class Vertex:       #this class is responsible for storing information about individual vertices
+  def __init__(self, value):
+    self.value = value
+    self.edges = {}
+
+  def add_edge(self, vertex, weight = 0):
+    self.edges[vertex] = weight
+
+  def get_edges(self):
+    return list(self.edges.keys())
